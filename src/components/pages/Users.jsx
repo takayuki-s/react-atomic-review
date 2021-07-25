d353b72cad91b709b88c670f8c68acde50e56f2e
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-07-26 08:48:29 +0900

    グリッドレイアウト適用

--- src/components/pages/Users.jsx
+++ src/components/pages/Users.jsx
@@ -21,9 +21,11 @@ export const Users = () => {
     <SContainer>
       <h2>ユーザー一覧</h2>
       <SearchInput />
-      {users.map((user) => (
-        <UserCard key={users.id} user={user} />
-      ))}
+      <SUserArea>
+        {users.map((user) => (
+          <UserCard key={users.id} user={user} />
+        ))}
+      </SUserArea>
     </SContainer>
   )
 }
@@ -33,4 +35,12 @@ const SContainer = styled.div`
   flex-direction: column;
   align-items: center;
   padding: 24px; 
+`
+
+const SUserArea = styled.div`
+  padding-top: 40px;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-gap: 20px;
 `
